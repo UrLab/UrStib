a4d42504764858c3ec2c89096a66085bc2ceb27e
Astramast <azerqsdfazerqsdg@gmail.com>
2025-03-06 22:36:48 +0100

    Thu  6 Mar 22:36:48 CET 2025

--- src/main.py
+++ src/main.py
@@ -11,9 +11,6 @@ def main():
 	stopfactory = StopFactory()
 	endpoint = StibAPIEndPoint(url, stopfactory)
 	coll = endpoint.get(query)
-	print("NEVER GONNA GIVE YOU UP")
-	print("NEVER GONNA LET YOU DOWN")
-	print("NEVER GONNA TURN AROUND AND HURT YOU")
 	for stop in coll:
 		print(stop.getName(), stop.getPointId())
 
