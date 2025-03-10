b9aa352c108b79ca0aa3e0b4077abf1923c28c7e
Astramast <azerqsdfazerqsdg@gmail.com>
2025-03-10 14:03:38 +0100

    Mon 10 Mar 14:03:38 CET 2025

---
+++ src/StibAPY/main.py
@@ -0,0 +1,8 @@
+from HttpGetter import HttpGetter
+
+link = "https://data.stib-mivb.be/api/explore/v2.1/catalog/datasets/waiting-time-rt-production/records?select=passingtimes&limit=100&offset=0&timezone=UTC&include_links=false&include_app_metas=false"
+
+response = HttpGetter().get(link).json()
+print(response["results"][0]["passingtimes"])
+print(type(response["results"][0]["passingtimes"]))
+
